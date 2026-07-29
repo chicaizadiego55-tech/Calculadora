f1f52baace8f83662cdf734b2c5e4668217a33c1
chicaizadiego55-tech <chicaizadiego55@gmail.com>
2026-07-28 19:27:25 -0500

    Agregar metodo multiplicar

---
+++ Calculadora.java
@@ -0,0 +1,13 @@
+package com.krakedev;
+
+public class Calculadora {
+
+    // Multiplicar
+    public double multiplicar(double valor1, double valor2) {
+        return valor1 * valor2;
+    }
+
+    
+    }
+
+
---
+++ TestCalculadora.java
@@ -0,0 +1,16 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+    public static void main(String[] args) {
+
+        Calculadora calc = new Calculadora();
+
+        double producto = calc.multiplicar(10, 5);
+        System.out.println("Multiplicación: " + producto);
+
+        
+
+    }
+
+}
